dd6cc10ba9f001ca4c8a653c9bd11ac8938e901f
Guilherme Tavares <guilhermet213@gmail.com>
2020-10-15 16:28:17 -0300

    Correçoes na navtabs

--- docs/public/scripts/scrolling.js
+++ docs/public/scripts/scrolling.js
@@ -9,7 +9,18 @@
 
     $('.nav-tabs')
       .scrollingTabs({
-        enableSwiping: true
+        scrollToTabEdge: true,
+        disableScrollArrowsOnFullyScrolled: true,
+        leftArrowContent:  [
+          '<div class="scrtabs-tab-scroll-arrow">',
+          '<span class="fa fa-arrow-left"></span>',
+         '</div>',
+        ].join(''), 
+        rightArrowContent:  [
+          '<div class="scrtabs-tab-scroll-arrow">',
+          '<span class="fa fa-arrow-right"></span>',
+         '</div>',
+        ].join('') 
       })
       .on('ready.scrtabs', function() {
         $('.tab-content').show();
